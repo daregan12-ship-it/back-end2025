03a8f74ffca9838c022a74555fa1267538f17248
Andres Aurelio Palmerin Torres <daregan12@gmail.com>
2025-12-01 14:16:46 -0600

    Configuracion de variables de entorno

---
+++ api/src/main/java/mx/tecnm/backend/api/controllers/ProductController.java
@@ -0,0 +1,23 @@
+package mx.tecnm.backend.api.controllers;
+
+import mx.tecnm.backend.api.modells.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return List.of(
+                new Product("Coca-cola", "1234567890", 20),
+                new Product("Squirt", "1234567891", 20),
+                new Product("Pepsi", "1234567892", 20)
+        );
+    }
+
+}
